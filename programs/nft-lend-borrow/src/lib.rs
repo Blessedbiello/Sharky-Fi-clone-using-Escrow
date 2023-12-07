18a8a27916e113a3588eb3a46da2ade933f2c261
blessedbiello <blessedbiello@gmail.com>
2023-12-07 21:14:29 +0100

    test, deployment

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("EvHuYVEgrSZWhqsLvqXL2KwWeHXRcSgVWbYMm4MdgB1G");
 
 #[program]
 pub mod nft_lend_borrow {
